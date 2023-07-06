97c164934012335720d5ea935fba73fe4a3a5620
Pierre Vueghs <pierre.vueghs@tobii.com>
2023-07-06 15:53:17 +0200

    Simplification du code pour fonctionner sur un seul point de données

--- Projets/Stellantis_1Input/Stellantis_1Input/ModelPredictor.cpp
+++ Projets/Stellantis_1Input/Stellantis_1Input/ModelPredictor.cpp
@@ -12,111 +12,65 @@ using namespace std;
 
 const double SQRT_2_PI = 2.506628274631000;
 
-ModelPredictor::ModelPredictor(/*size_t theNumberOfTestData,*/
-                               AbstractModel *theModel)
-    : /*myNumberOfTestData(theNumberOfTestData), */myModel(theModel) {}
+ModelPredictor::ModelPredictor(AbstractModel *theModel)
+    : myModel(theModel) {}
 
 int ModelPredictor::Predict(bool theHasFileName, double* parameters, int nbParameters) {
-  // Compute scores
-  Matrix *scores = GetScores(parameters, nbParameters);
+    // Compute scores
+    std::vector<double> scores = GetScores(parameters, nbParameters);
 
-  // Compute soft max
-  Matrix *aSoftMax = GetSoftMax(scores);
+    // Compute soft max
+    std::vector<double> aSoftMax = GetSoftMax(scores);
 
-  // GetCost
-  Matrix *aCost = GetCost(aSoftMax);
+    // GetCost
+    std::vector<double> aCost = GetCost(aSoftMax);
 
-  // Export results in CSV file
-  int zozo = ExportResultsInCSV(theHasFileName, aCost);
-
-  return zozo;
+    // Export results in CSV file
+    return ExportResultsInCSV(theHasFileName, aCost);
 }
 
-Matrix *ModelPredictor::GetCost(const Matrix *theSoftMax) {
+std::vector<double> ModelPredictor::GetCost(const std::vector<double>& theSoftMax) {
   // Taking advantage of the Cost matrix, which is 1 everywhere and zero on the
   // diagonal
-  Matrix *aCost = new Matrix(1, 5);
+  std::vector<double> aCost = { 0., 0., 0., 0., 0. };
   for (size_t ii(0); ii < 5; ++ii) {
-    //for (size_t jj(0); jj < myNumberOfTestData; ++jj) {
       double aTempValue(0.);
       for (size_t kk(0); kk < 5; ++kk) {
-        if (kk != ii)
-          aCost->Append(0, ii, theSoftMax->Get(0, kk));
+          if (kk != ii)
+              aCost[ii] += theSoftMax[kk];
       }
-    //}
   }
   return aCost;
 }
 
 int ModelPredictor::ExportResultsInCSV(bool theHasFileName,
-                                        const Matrix *theCost) const {
+                                        const std::vector<double>& theCost) const {
   //// Get index of highest probability class
-  //std::vector<double> prior = myModel->getPrior();
-  //size_t index =
-  //    std::distance(begin(prior), std::max_element(begin(prior), end(prior)));
-
-  //std::vector<size_t> labels;
-  //for (size_t ii(0); ii < myNumberOfTestData; ++ii) {
-  //  labels.push_back(index);
-  //}
-
-  //ofstream myfile;
-  //myfile.open("results.csv");
-  //std::vector<std::string> anIds =
-  //    LoadTestData::getInstance()->GetInputDataForExport(
-  //        InputDataForExport::IDFE_id);
-  //std::vector<std::string> aTimestamps =
-  //    LoadTestData::getInstance()->GetInputDataForExport(
-  //        InputDataForExport::IDFE_Timestamp);
-  //std::vector<std::string> aStates =
-  //    LoadTestData::getInstance()->GetInputDataForExport(
-  //        InputDataForExport::IDFE_State);
-  //std::vector<std::string> aLoDs =
-  //    LoadTestData::getInstance()->GetInputDataForExport(
-  //        InputDataForExport::IDFE_LoD);
-  //std::vector<std::string> aClassNames = myModel->GetClassNames();
-
-  //myfile << "Index;id;Timestamp;Output;State;LoD\n";
-
-  //for (size_t ii(0); ii < myNumberOfTestData; ++ii) {
-    double aMinValue = theCost->Get(0, 0);
+    double aMinValue = theCost[0];// ->Get(0, 0);
     size_t minIndex(0);
     for (size_t jj(1); jj < 5; ++jj) {
-      if (theCost->Get(0, jj) < aMinValue) {
-        aMinValue = theCost->Get(0, jj);
+      if (theCost[jj] < aMinValue) {
+        aMinValue = theCost[jj];
         minIndex = jj;
       }
     }
     return minIndex;
-    //labels.at(ii) = minIndex;
-    // cout << "labels " << ii << " " << minIndex << ";" << endl;
-    //if (theHasFileName) {
-    //  myfile << ii << "," << anIds.at(ii) << "," << aTimestamps.at(ii) << ","
-    //         << aClassNames.at(minIndex) << "," << aStates.at(ii) << ","
-    //         << aLoDs.at(ii) << "\n";
-    //} else {
-      //myfile << ii << ", , ," << aClassNames.at(minIndex) << ",,\n";
-    //}
-  //}
-  //myfile.close();
 }
 
-Matrix *ModelPredictor::GetSoftMax(const Matrix *theScores) {
+std::vector<double> ModelPredictor::GetSoftMax(const std::vector<double>& theScores) {
   double aTemparray[5] = {0.};
 
-  // ArrayStruct *aSoftMax = new ArrayStruct();
-  Matrix *aSoftMax = new Matrix(1, 5);
+  std::vector<double> aSoftMax;
 
   // Soft max on scores
-  //for (size_t ii(0); ii < myNumberOfTestData; ++ii) {
-    double aMaxValue(theScores->Get(0, 0));
+    double aMaxValue(theScores[0]);
     for (size_t jj(1); jj < 5; ++jj) {
-      if (theScores->Get(0, jj) > aMaxValue)
-        aMaxValue = theScores->Get(0, jj);
+      if (theScores[jj] > aMaxValue)
+        aMaxValue = theScores[jj];
     }
 
     for (size_t jj(0); jj < 5; ++jj) {
-      aTemparray[jj] = std::exp(theScores->Get(0, jj) - aMaxValue);
+      aTemparray[jj] = std::exp(theScores[jj] - aMaxValue);
     }
 
     double aLValue = aTemparray[0];
@@ -126,9 +80,8 @@ Matrix *ModelPredictor::GetSoftMax(const Matrix *theScores) {
     aLValue = std::log(aLValue) + aMaxValue;
 
     for (size_t jj(0); jj < 5; ++jj) {
-      aSoftMax->Set(0, jj, std::exp(theScores->Get(0, jj) - aLValue));
+        aSoftMax.push_back(std::exp(theScores[jj] - aLValue));
     }
-  //}
   return aSoftMax;
 }
 
@@ -150,8 +103,8 @@ size_t findNextGreaterThan(const std::vector<double> &data, double value,
   return idx;
 }
 
-Matrix *ModelPredictor::GetScores(double* parameters, int nbParameters) {
-  Matrix *score = new Matrix(1, 5);
+std::vector<double> ModelPredictor::GetScores(double* parameters, int nbParameters) {
+  std::vector<double> scores = { 0., 0., 0., 0., 0. };
 
   int aNumberOfPredictors(myModel->GetNumberOfPredictors());
 
@@ -174,23 +127,10 @@ Matrix *ModelPredictor::GetScores(double* parameters, int nbParameters) {
       size_t jstart(0), jend(0);
       double halfWidth = 4. * distributionParameters.bandWidth;
 
-      double aPredictorTestInput = parameters[jj];
-
-      //std::vector<std::pair<size_t, double>> aPredictorTestInput(
-      //    LoadTestData::getInstance()->GetTestDataForPredictor(jj));
-
-      //std::stable_sort(begin(aPredictorTestInput), end(aPredictorTestInput),
-      //                 [](const auto &val1, const auto &val2) {
-      //                   return val1.second < val2.second;
-      //                 });
-
-      //for (size_t ll(0); ll < aPredictorTestInput.size(); ++ll) {
         // Add log prior to corresponding score, but only once
-        if (jj == 0)
-            score->Append(0, ii, aLogPrior);
-        //score->Append(aPredictorTestInput.at(ll).first - 1, ii, aLogPrior);
-        //double xi = aPredictorTestInput.at(ll).second;
-        double xi = aPredictorTestInput;
+      if (jj == 0)
+          scores[ii] = aLogPrior;
+        double xi = parameters[jj];
 
         double aLowValue(xi - halfWidth);
         jstart = findNextGreaterOrEqual(distributionParameters.inputData,
@@ -220,10 +160,9 @@ Matrix *ModelPredictor::GetScores(double* parameters, int nbParameters) {
         fk = std::log(fk);
 
         // Add log(fk) to corresponding score
-        score->Append(0, ii, fk);
-      //}
+        scores[ii] += fk;
     }
   }
 
-  return score;
+  return scores;
 }
--- Projets/Stellantis_1Input/Stellantis_1Input/Stellantis.cpp
+++ Projets/Stellantis_1Input/Stellantis_1Input/Stellantis.cpp
@@ -25,55 +25,6 @@ int main()
 	std::cout << "LoD " << zozo << std::endl;
 }
 
-
-
-//#include <iostream>
-//#include <string>
-//
-//int main(int argc, char *argv[]) {
-//  bool aHasFileName(false);
-//  AbstractModel *aModel = nullptr;
-//  if (argc < 2) {
-//    std::cout
-//        << "Missing input file. Running pressure model on default test set"
-//        << std::endl;
-//
-//    LoadTestData::getInstance()->InitializeTestData();
-//    aModel = new PressureModel();
-//    LoadTestData::getInstance()->SetModel(aModel);
-//  } else
-//  // C:\\Users\\pv2077\\Documents\\Stellantis\\Projets\\Stellantis\\x64\\Debug\\features_150s_normalized_sliding90s.csv
-//  {
-//    aHasFileName = true;
-//    std::string aFileName(argv[1]);
-//    if (aFileName.size() < 4  || aFileName.substr(aFileName.length()-4, 4) != ".csv")
-//    {
-//        std::cout << "Invalid first argument.\nFirst argument must be the full path to a .csv file, where separator is comma ','." << std::endl;
-//        return -1;
-//    }
-//    LoadTestData::getInstance()->SetFileName(aFileName);
-//    if (argc == 3) {
-//      std::cout << "Using input file " << aFileName
-//                << "\nRunning physiological model on input test set"
-//                << std::endl;
-//      aModel = new PhysiologicalModel();
-//      LoadTestData::getInstance()->SetModel(aModel);
-//    } else {
-//      std::cout << "Using input file " << aFileName
-//                << "\nRunning pressure model on input test set" << std::endl;
-//      aModel = new PressureModel();
-//      LoadTestData::getInstance()->SetModel(aModel);
-//    }
-//    LoadTestData::getInstance()->LoadCsvFile();
-//    LoadTestData::getInstance()->ReLoadCsvFile();
-//  }
-//
-//  size_t aNumberOfTestData(LoadTestData::getInstance()->GetNumberOfTestData());
-//
-//  ModelPredictor aModelPredictor(aNumberOfTestData, aModel);
-//  aModelPredictor.Predict(aHasFileName);
-//}
-
 int ComputeLevelOfDrowsiness(double* parameters, int nbParameters)
 {
 	AbstractModel* aModel(nullptr);
